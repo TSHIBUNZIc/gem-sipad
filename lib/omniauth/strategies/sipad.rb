35fb27ec9e3ed0a397ae7c99016973849be3e3ae
Nicolas DECHELETTE <n.dechelette@sipadconnect.fr>
2021-06-29 17:22:20 +0200

    finished

--- lib/omniauth/strategies/sipad.rb
+++ lib/omniauth/strategies/sipad.rb
@@ -5,29 +5,24 @@ module OmniAuth
   module Strategies
     class Sipad  < OmniAuth::Strategies::OAuth2
       # include OmniAuth::Strategy
-      args [:consumer_key, :consumer_secret]
+      option :name, "sipad"
       # definir nos arguments
 
       option :client_options, {
-        site: 'https://www.sipadconnect.fr',
-        # raw_info_url: 'https://apifeed.sellsy.com/0/', #
+        site: 'http://127.0.0.1:5000/',
+        raw_info_url: '/my/profile.json', #
         # request_token_path: '/0/request_token',
         # access_token_path: '/0/access_token',
         # authorize_path: '/0/login.php'
         authorize_path: '/users/sign_in'
       }
 
-      credentials do
-        {
-          token:  access_token.token,
-          secret: access_token.secret
-        }
-      end
+      option :pkce, true
 
       info do
         {
           id:         raw_info['id'],
-          name:       "#{raw_info['surname']} #{raw_info['first_name']}" ,
+          name:       "#{raw_info['surname']} #{raw_info['firstname']}" ,
           email:      raw_info['email'],
           home_phone:      raw_info['phone_number']
         }
@@ -44,13 +39,15 @@ module OmniAuth
       private
 
       def raw_info
-        @raw_info ||= JSON.parse(access_token.post(options[':users']['raw_info_url'], { 'do_in' => {
-            'method' => 'Infos.getInfos',
-            'params' => []
-          }.to_json,
-          'io_mode' => 'json',
-          'request' => 1
-        }).body)['response']['userdatas']
+        @raw_info ||= access_token.get(options['client_options']['raw_info_url']).parsed
+        # @raw_info ||= JSON.parse(access_token.post(options['client_options']['raw_info_url'], { 'do_in' => {
+        #   'method' => 'Infos.getInfos',
+        #   'params' => []
+        # }.to_json,
+        # 'io_mode' => 'json',
+        # 'request' => 1
+        # }).body)['response']['userdatas']
+
       end
     end
   end
